053f4bfe01c62678e817d2b5773eabc3249d381b
fangyong2 <40332616+fangyong2@users.noreply.github.com>
2021-08-17 08:55:00 +0800

    Add files via upload

---
+++ JDBC/JDBCUtil.java
@@ -0,0 +1,73 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+public class JDBCUtil {
+
+    private static JDBCUtil singleton = null;
+    private static String DRIVERNAME = null;
+    private static String URL = null;
+    private static String USER = null;
+    private static String PASSWORD = null;
+
+//    private  Connection conn = null;
+
+    static {
+        try {
+
+            Properties props = new Properties();
+            //Reader in = new FileReader("db.properties");
+            InputStream in = JDBCUtil.class.getClassLoader().getResourceAsStream("db.properties");
+            props.load(in);
+
+            DRIVERNAME = props.getProperty("drivername");
+            URL = props.getProperty("url");
+            USER = props.getProperty("user");
+            PASSWORD = props.getProperty("password");
+
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+
+        }
+    }
+
+
+    public static JDBCUtil getInstance() {
+
+        if (singleton == null) {
+
+            singleton = new JDBCUtil();
+
+        }
+
+        return singleton;
+
+    }
+    public  Connection getConnection() throws Exception {
+//        if (conn != null) {
+//            return conn;
+//        }
+
+        Class.forName(DRIVERNAME);
+        Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        return conn;
+    }
+
+    public  void closeResource(Connection conn, PreparedStatement st) throws SQLException {
+        st.close();
+        conn.close();
+    }
+
+    public  void closeResource(Connection conn,  PreparedStatement st,ResultSet rs) throws SQLException {
+        st.close();
+        rs.close();
+        conn.close();
+    }
+}
